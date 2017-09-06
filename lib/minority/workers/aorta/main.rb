f2be53cae65358bd9eec3d9a74c1d5797dab72bd
P. G <pg@akcjademokracja.pl>
2017-09-06 15:24:10 +0200

    Let's get this dirty hack running.

--- lib/minority/workers/aorta/main.rb
+++ lib/minority/workers/aorta/main.rb
@@ -5,7 +5,7 @@ class AortaMainWorker
 
     def perform
 
-        # Make a connection first
+        # Estabilish a connection first
         begin
             @conn = Bunny.new(ENV['AMQP_URL'])
             @conn.start
@@ -18,20 +18,30 @@ class AortaMainWorker
             ch = @conn.create_channel
             q = ch.queue(ENV['AMQP_QUEUE_NAME'], durable: true)
 
-            q.subscribe(block: true, manual_ack: true) do |delivery_info, properties, payload|
+            # Don't do anything if the queue is empty. If it wasn't for that, we'd have too much MainWorkers
+            # which would then throw Timeout::Errors.
+            unless q.message_count == 0
+                # If it was block: false, it'd spawn the consumer in an another thread...
+                # and since this worker is run every 5 minutes, you'd get another thread every 5 minutes.
+                # The subscribers would basically exist and be idle somewhere, waiting for messages.
+                q.subscribe(block: true, manual_ack: true) do |delivery_info, properties, payload|
                 message_type, message_details = JSON.parse(payload)
 
-                case message_type
-                when "freshdesk_check_ticket"
-                        # Let's just acknowledge this message so that RabbitMQ doesn't requeue it
-                        # After acknowledging, delegate the work to a specified worker
+                    case message_type
+                    when "freshdesk_check_ticket"
+                            # Let's just acknowledge this message so that RabbitMQ doesn't requeue it
+                            # After acknowledging, delegate the work to a specified worker
 
-                        ch.ack(delivery_info.delivery_tag)
-                        AortaCheckTicketWorker.perform_async(message_details["ticket_id"])
-                else
-                end
+                            ch.ack(delivery_info.delivery_tag)
+                            AortaCheckTicketWorker.perform_async(message_details["ticket_id"])
+                    else
+                    end
                 
-            end            
+                end
+            else
+                return
+            end
+
         rescue Bunny::PreconditionFailed => e
             puts "Channel-level exception! Code: #{e.channel_close.reply_code}, message: #{e.channel_close.reply_text}".squish
         ensure
