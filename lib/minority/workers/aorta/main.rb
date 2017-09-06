6d4e1a0204b221a04c1f35dadf22a28e6f2bccf0
P. G <pg@akcjademokracja.pl>
2017-09-06 12:08:55 +0200

    Attempted to make sure that AortaMainWorker doesn't pile up open channels

--- lib/minority/workers/aorta/main.rb
+++ lib/minority/workers/aorta/main.rb
@@ -1,31 +1,42 @@
 require 'bunny'
 
 class AortaMainWorker
-	include Sidekiq::Worker
+    include Sidekiq::Worker
 
-	def initialize
-		@conn = Bunny.new(ENV['AMQP_URL'])
-	end
+    def perform
 
-	def perform
-		@conn.start
-		ch = @conn.create_channel
-		q = ch.queue(ENV['AMQP_QUEUE_NAME'], {durable: true})
+        # Make a connection first
+        begin
+            @conn = Bunny.new(ENV['AMQP_URL'])
+            @conn.start
+        rescue Bunny::TCPConnectionFailed => e
+            puts "Connection to RabbitMQ failed!"
+        end
 
-			q.subscribe(block: false, manual_ack: true) do |delivery_info, properties, payload|
-				message_type, message_details = JSON.parse(payload)
+        # Process
+        begin
+            ch = @conn.create_channel
+            q = ch.queue(ENV['AMQP_QUEUE_NAME'], durable: true)
 
-				case message_type
-				when "freshdesk_check_ticket"
-					# Let's just acknowledge this message so that RabbitMQ doesn't requeue it
-					# After acknowledging, delegate the work to a specified worker
+            q.subscribe(block: false, manual_ack: true) do |delivery_info, properties, payload|
+                message_type, message_details = JSON.parse(payload)
+            end
 
-					ch.ack(delivery_info.delivery_tag)
-					AortaCheckTicketWorker.perform_async(message_details["ticket_id"])
-				else
-				end
+            case message_type
+                when "freshdesk_check_ticket"
+                    # Let's just acknowledge this message so that RabbitMQ doesn't requeue it
+                    # After acknowledging, delegate the work to a specified worker
 
-			end
-	end
+                    ch.ack(delivery_info.delivery_tag)
+                    AortaCheckTicketWorker.perform_async(message_details["ticket_id"])
+                else
+            end
+        rescue Bunny::PreconditionFailed => e
+            puts "Channel-level exception! Code: #{e.channel_close.reply_code}, message: #{e.channel_close.reply_text}".squish
+        ensure
+            ch.close
+            @conn.close
+        end
 
+    end
 end
