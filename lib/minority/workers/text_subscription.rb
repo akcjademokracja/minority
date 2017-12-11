a69348da0e8b94ae307c7e147c397071ce42eb43
Marcin Koziej <marcin@akcjademokracja.pl>
2017-12-11 16:20:09 +0100

    Add worker to subscribe to SMS people with phones
    Only subscribes members if they do not have any sms subscription

---
+++ lib/minority/workers/text_subscription.rb
@@ -0,0 +1,15 @@
+class DonationFromActionWorker
+  include Sidekiq::Worker
+
+  def perform()
+    sms = Subscription.find Subscription::SMS_SUBSCRIPTION
+    m = Member.joins(:phone_numbers).
+      joins(%Q{LEFT JOIN member_subscriptions
+               ON member_subscriptions.member_id = members.id
+               AND member_subscriptions.subscription_id = #{sms.id}}).
+      where(member_subscriptions: { id: nil })
+    m.each do |m|
+      m.subscribe_to sms
+    end
+  end
+end
