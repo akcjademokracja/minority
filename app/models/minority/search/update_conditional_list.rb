d9b59556a8af0e8d70a537157e62c974d0b3ec85
Marcin Koziej <marcin@akcjademokracja.pl>
2018-02-14 13:52:37 +0100

    Use Concerns also for simple methods [for uniformity]

--- app/models/minority/search/update_conditional_list.rb
+++ app/models/minority/search/update_conditional_list.rb
@@ -1,20 +1,24 @@
 module Minority
   module Search
     module UpdateConditionalList
-  	  def update_conditional_list(list_name)
-  		  conditional = ConditionalList.find_or_create_by(name: list_name)
+      extend ActiveSupport::Concern
 
-  		  ActiveRecord::Base.transaction do
-  			  conditional.conditional_list_members.delete_all
-      	  result = RedshiftDB.connection.execute(sql)
-      	  total_count = result.count
+      included do
+  	    def update_conditional_list(list_name)
+  		    conditional = ConditionalList.find_or_create_by(name: list_name)
 
-      	  result.each_slice(10_000) do |slice|
-        	  values = slice.map { |row| "(#{row['id']}, #{conditional.id}, NOW(), NOW())" }
-        	  ActiveRecord::Base.connection.execute(%Q{
+  		    ActiveRecord::Base.transaction do
+  			    conditional.conditional_list_members.delete_all
+      	    result = RedshiftDB.connection.execute(sql)
+      	    total_count = result.count
+
+      	    result.each_slice(10_000) do |slice|
+        	    values = slice.map { |row| "(#{row['id']}, #{conditional.id}, NOW(), NOW())" }
+        	    ActiveRecord::Base.connection.execute(%Q{
 					INSERT INTO conditional_list_members (member_id, conditional_list_id, created_at, updated_at)
 					VALUES #{values.join(',')}
 				})
+            end
           end
         end
   	  end
