4bebffa84c239e9cda6dc95eec018918ebcb722c
P. G <pg@akcjademokracja.pl>
2018-02-14 13:39:34 +0100

    Improved file loading

--- lib/minority.rb
+++ lib/minority.rb
@@ -2,21 +2,11 @@ require "minority/engine"
 
 module Minority
 	def self.load_files
-		[
-			"app/workers/donation_from_action",
-			"app/workers/aorta/main",
-			"app/workers/aorta/check_ticket",
-			"app/workers/bank_acct/bank_payment_import",
-			"app/workers/controlshift_cache_categorizations",
-			"app/workers/controlshift_categorize",
-			"app/workers/controlshift_categorize_one",
-			"app/services/gdpr"
-		]
+		Dir.chdir(File.join(File.dirname(__FILE__), '..')) { Dir['app/workers/**/*.rb'] + Dir['app/services/**/*.rb'] }
 	end
 
 	def self.motd
-		if ENV['RACK_ENV'] == 'development'
-      		puts %q[
+      	puts %q[
                            .-"""""-.
                           (('     `))
   Loading Minority...    .'`-.....-'`.
@@ -26,7 +16,6 @@ module Minority
                         \  `&'    `%' /
                          `..:%.:::.7.'
                            `"""""""'
-			]
-    	end
+				]
 	end
 end
--- lib/minority/engine.rb
+++ lib/minority/engine.rb
@@ -2,11 +2,11 @@ module Minority
   class Engine < ::Rails::Engine
     # isolate_namespace Minority
     initializer "minority", after: :load_config_initializers do |app|
+    	Minority.motd
   		Minority.load_files.each do |file|
+  			puts "Loading module: #{file.to_s} "
     		require_relative File.join("../..", file)
   		end
-
-  		Minority.motd
 	end
   end
 end
