2ab0d8b4a00b64e8621c096931e4be3674e174a9
Marcin Koziej <marcin@akcjademokracja.pl>
2018-08-08 12:45:38 +0200

    Make ghosting condition code more readable

--- app/workers/ghostbuster.rb
+++ app/workers/ghostbuster.rb
@@ -8,11 +8,13 @@ class GhostbusterWorker
       next if m.donations.count >= 1 or m.subscribed?
 
       # If there are no member_actions with member_action_consents of public_id 'gdpr_campaign_1.0' and and campaign that is not finished, ghost the member.
-      GhostMemberWorker.perform_async(m.id) if m.member_actions.joins(member_action_consents: :consent_text)
-                                                               .where(member_action_consents: { consent_texts: { public_id: 'gdpr_campaign_1.0' } })
-                                                               .joins(action: :campaign)
-                                                               .where(action: { campaigns: { finished_at: nil } })
-                                                               .empty? 
+      next unless m.member_actions.joins(member_action_consents: :consent_text)
+                    .where(member_action_consents: { consent_texts: { public_id: 'gdpr_campaign_1.0' } })
+                    .joins(action: :campaign)
+                    .where(action: { campaigns: { finished_at: nil } })
+                    .empty?
+
+      GhostMemberWorker.perform_async(m.id)
     end
   end
 end
